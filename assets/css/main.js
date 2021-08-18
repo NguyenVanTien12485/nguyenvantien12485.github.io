8d5b54e15e7c7e9de3394accbc3e0a2049e7095c
NguyenVanTien12485 <88303673+NguyenVanTien12485@users.noreply.github.com>
2021-08-18 15:51:23 +0700

    upload

---
+++ assets/css/main.js
@@ -0,0 +1,23 @@
+// var $Hoc_JS = 'Học Javascript';
+// var upper = $Hoc_JS.replace(' ','')
+// var array =[];
+// for(var i=0 ; i<upper.length;i++){
+//   array[i] = upper[i]
+// }
+// console.log(array)
+
+// -----
+// var str ='x8i2n 7ch5a3o';
+// var strOut='', num='';
+// for(var i=0; i<str.length; i++){
+//  if (!isNaN(str[i]) && str[i]!==' '){
+//    num+=str[i]
+//  } else {
+//    strOut+=str[i]
+//  }
+// }
+// console.log(strOut)
+// console.log(num)
+// console.log(typeof num)\
+
+const buyBtns = document.querySelectorAll('.js-buy-ticket')
